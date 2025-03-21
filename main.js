d80d80df2ecfe0b5f288da914d4385dc2c7477ab
Mateo-Paternina-Mercado <185979424+Mateo-Paternina-Mercado@users.noreply.github.com>
2025-03-21 12:39:21 -0500

    fix: :bug: Add identificar variable and loop to main.js
    It's the only thing he gave me to do, I couldn't think

---
+++ main.js
@@ -0,0 +1,14 @@
+const identificar = ("()", "{}", "[]");
+
+while (identificar) {
+    if (true){
+        console.log("()", "{}", "[]");
+        return true
+    }else if(false){
+        console.log("()", "{}", "[]");
+        return false
+    }
+    
+}
+
+console.log(identificar);
